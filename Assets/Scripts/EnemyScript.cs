a00233443f62d38df17dd5e0bbc51bee1ee29395
Elliott Smith <Elliott2.Smith@live.uwe.ac.uk>
2017-01-24 11:34:31 +0000

    Enemies!!!!

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+    public float positionOne = 0;
+    public float positionTwo = 0;
+    bool moveDirection = false;
+    public bool UpDown = false;
+    public bool rotating = false;
+    public int speed = 1;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        checkPosition();
+        moveEnemy();
+	
+	}
+
+    void moveEnemy()
+    {
+        if (!UpDown)
+        {
+            if (moveDirection)
+            {
+                transform.position += Vector3.left * -speed * Time.deltaTime;
+            }
+
+            else
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+        }
+
+        else
+        {
+            if (moveDirection)
+            {
+                transform.position += Vector3.up * -speed * Time.deltaTime;
+            }
+
+            else
+            {
+                transform.position += Vector3.up * speed * Time.deltaTime;
+            }
+        }
+
+        if (rotating)
+        {
+            transform.Rotate(Vector3.forward * 30);
+        }
+    }
+
+    void checkPosition()
+    {
+        if (!UpDown)
+        {
+            if (transform.position.x < positionOne)
+            {
+                moveDirection = true;
+            }
+
+            else if (transform.position.x > positionTwo)
+            {
+                moveDirection = false;
+            }
+        }
+
+        else
+        {
+            if (transform.position.y > positionOne)
+            {
+                moveDirection = true;
+            }
+
+            else if (transform.position.y < positionTwo)
+            {
+                moveDirection = false;
+            }
+        }
+    }
+}
