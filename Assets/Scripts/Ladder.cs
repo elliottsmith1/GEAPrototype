3d8a55ab8d39550378eb81543adac4357350d9d6
P-Lubinski <piotrek_94@hotmail.com>
2017-01-25 14:34:23 +0000

    I've been working on few things, I slightly changed how the one-way-platform behaves (not the script itself, just colliders and stuff) I made some new prefabs for platforms I will need to actually design the level, I also added sliders to platforms (you need them to avoid getting stuck when jumping from one platform to another) I reworked the ladder script (however I didn't delete the previous stuff, just commented it out), now you can hold W to climb and hold S to go down the ladder, I will need to figure out how gravity works (in Unity.. ><) to stop player from moving if he just sits on the ladder (at the moment he starts to fall slowly, I found a way to reverse that, but not to stop it) As with everything else - I will do more testing once I have the rest of the assets available to me, at the moment I'm doing tests with the pre-made 2D character Unity provides. Now I'll actually start creating the level but I wanted to upload these new prefabs in case anyone needed them (or wanted to test them) Also: big warning! I don't think Unity has an auto-save feature... so... spam that Ctrl+S unless you want to loose a hours worth of progress like I did ;p

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -1,31 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Ladder : MonoBehaviour {
-    /*private PlayerMovement player;
+public class Ladder : MonoBehaviour
+{
+    //private PlayerMovement player;
+    public float speed = 6;
+
 	// Use this for initialization
-	void Start () {
-        player = FindObjectOfType<PlayerMovement>();
+	void Start ()
+    {
+        //player = FindObjectOfType<PlayerMovement>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.tag == "Player")
-        {
-            player.on_ladder = true;
-        }
-    }
+    //void OnTriggerEnter2D(Collider2D col)
+    //{
+    //    if (col.tag == "Player")
+    //    {
+    //        player.on_ladder = true;
+    //    }
+    //}
 
-    void OnTriggerExit2D(Collider2D col)
+    //void OnTriggerExit2D(Collider2D col)
+    //{
+    //    if (col.tag == "Player")
+    //    {
+    //        player.on_ladder = false;
+    //    }
+    //}
+
+    void OnTriggerStay2D(Collider2D other)
     {
-        if (col.tag == "Player")
+        if (other.tag=="Player" && Input.GetKey(KeyCode.W))
         {
-            player.on_ladder = false;
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
         }
-    }*/
+            
+       else if (other.tag == "Player" && Input.GetKey(KeyCode.S))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+        }
+
+        else
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }             
+     }
 }
