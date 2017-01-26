8a7dd34026b3d3239b32a1920940200da362e7ed
callan-patay <Callan2.Patay@live.uwe.ac.uk>
2017-01-26 14:36:54 +0000

    Updated Rope
    still quite janky.. trying to get a smooth movement but at least it isnt

---
+++ Assets/Scripts/ForceWeight.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceWeight : MonoBehaviour {
+
+
+    public bool direction;
+    private Rigidbody2D weightBody;
+
+	// Use this for initialization
+	void Start () {
+        direction = true;
+        weightBody = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("addForce", 0.0f, 0.80f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void addForce()
+    {
+        if(direction)
+        {
+           weightBody.AddForce(transform.right * 5000);
+        }
+        else
+        {
+            weightBody.AddForce(transform.right * -5000);
+        }
+
+        direction = !direction;
+    }
+}
