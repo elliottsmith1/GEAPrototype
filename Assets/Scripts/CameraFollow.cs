876dd9c5f9e852442298874c3b251da0a5f8f666
Elliott Smith <elliott2.smith@live.uwe.ac.uk>
2017-01-30 16:25:52 +0000

    added script to main camera to follow player on X axis

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject target;
+
+	void Start()
+    {
+	    target = GameObject.Find("Player");
+	}
+	
+	void Update()
+    {
+        Vector3 temp = target.transform.position;
+        temp.z = -10;
+        temp.y = 4;
+        transform.position = temp;
+	}
+}
