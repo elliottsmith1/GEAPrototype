24201080aa88ab5aa329cb4197dfc95337cc7d72
Elliott Smith <Elliott2.Smith@live.uwe.ac.uk>
2017-02-01 16:11:40 +0000

    collectables now change colour :3

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -5,12 +5,19 @@ public class Collectable : MonoBehaviour {
 
     public GUIText scoreText;
     private int score;
+    private int spriteNum = 4;
+    public Sprite grey;
+    public Sprite green;
+    public Sprite blue;
+    public Sprite red;
+    public Sprite yellow;
 
     // Use this for initialization
     void Start ()
     {
         score = 10;
         UpdateScore();
+        InvokeRepeating("ChangeSprite", 0.2f, 0.2f);
     }
 	
 	// Update is called once per frame
@@ -36,5 +43,38 @@ public class Collectable : MonoBehaviour {
     void UpdateScore()
     {
         scoreText.text = "Gems to oollect: " + score;
+    }
+
+    void ChangeSprite()
+    {
+        if (spriteNum == 1)
+        {
+            GetComponent<SpriteRenderer>().sprite = grey;
+            spriteNum = 2;
+        }
+
+        else if (spriteNum == 2)
+        {
+            GetComponent<SpriteRenderer>().sprite = green;
+            spriteNum = 3;
+        }
+
+        else if (spriteNum == 3)
+        {
+            GetComponent<SpriteRenderer>().sprite = blue;
+            spriteNum = 4;
+        }
+
+        else if (spriteNum == 4)
+        {
+            GetComponent<SpriteRenderer>().sprite = red;
+            spriteNum = 5;
+        }
+
+        else if (spriteNum == 5)
+        {
+            GetComponent<SpriteRenderer>().sprite = yellow;
+            spriteNum = 1;
+        }
     }
 }
